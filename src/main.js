cf75289bb5535460d96a1546c046b0649ee2fb02
mimmos91 <mimmos@hanmail.net>
2024-12-03 22:18:58 +0900

    change router setting

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
-import PADA from '../views/PADA.vue'
-import PWD from '../views/PWD.vue'
-import ManagerMain from '../views/manager/ManagerMain.vue'
-import Register from '../views/manager/Register.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'login',
-    component: Login,
-  },
-  {
-    path: '/pada',
-    name: 'PADA',
-    component: PADA,
-  },
-  {
-    path: '/pwd',
-    name: 'PWD',
-    component: PWD,
-  },
-  {
-    path: '/manager',
-    name: 'ManagerMain',
-    component: ManagerMain,
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register,
-  },
-]
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/pada',
+      name: 'pada',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PADA.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Login.vue'),
+    },
+  ],
 })
 
 export default router
